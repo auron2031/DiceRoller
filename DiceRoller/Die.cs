937b486fb2d2fda3d5bbf459c7fbf8d6139b2305
TDCheese <haha@haha.ha>
2018-01-10 14:07:12 -0800

    Add project files.

---
+++ DiceRoller/Die.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    /// <summary>
+    /// Represents a standard single 6-sided dice
+    /// </summary>
+    class Die
+    {
+        private static Random random;
+        /// <summary>
+        /// Creates a die with a random value
+        /// </summary>
+        public Die()
+        {
+            random = new Random();
+            Roll();
+        }
+
+        public byte Value { get; private set; }
+        /// <summary>
+        /// Rolls the die and generates a new 7
+        /// </summary>
+        public void Roll()
+        {
+            //Generate a new random value
+            Random rand = new Random();
+            int val = rand.Next(1, 7);
+            Value = Convert.ToByte(val);
+        }
+    }
+}
---
+++ DiceRoller/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Die[] dice = new Die[5];
+
+            //Create 5 die
+            for (int i = 0; i < dice.Length; i++)
+            {
+                dice[i] = new Die();
+            }
+
+            //Print die values
+            for (int i = 0; i < dice.Length; i++)
+            {
+                Console.WriteLine(dice[i].Value);
+            }
+            Console.ReadKey();
+        }
+    }
+}
